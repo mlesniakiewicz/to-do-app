953c10d5efe112cbad3d97e2537cb8632aea8028
Michal L <michal.lesniakiewicz@gmail.com>
2019-10-02 14:22:31 +0200

    Updates

---
+++ precache-manifest.a16e5784c8f6e08b932e2f1aa07a7087.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f088219ad09933e3e00315a64820310",
+    "url": "/to-do-app/static/media/noisy_net.3f088219.png"
+  },
+  {
+    "revision": "b1f23a16b0726314c12b50d9eecacdd9",
+    "url": "/to-do-app/static/media/green-fibers.b1f23a16.png"
+  },
+  {
+    "revision": "a43a70eaa762ba0b6430aaad23607ec0",
+    "url": "/to-do-app/static/media/concrete_wall.a43a70ea.png"
+  },
+  {
+    "revision": "60e6816a297c28fee7c8",
+    "url": "/to-do-app/static/js/runtime~main.60e6816a.js"
+  },
+  {
+    "revision": "2ec1619a5b5efa6bac8f",
+    "url": "/to-do-app/static/js/main.2ec1619a.chunk.js"
+  },
+  {
+    "revision": "f6b84cc0c44b8101ab3f",
+    "url": "/to-do-app/static/js/1.f6b84cc0.chunk.js"
+  },
+  {
+    "revision": "2ec1619a5b5efa6bac8f",
+    "url": "/to-do-app/static/css/main.a2d39c6a.chunk.css"
+  },
+  {
+    "revision": "543b2c192405739b680550d9b9358a55",
+    "url": "/to-do-app/index.html"
+  }
+];
